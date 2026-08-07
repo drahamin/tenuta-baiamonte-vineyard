44c2e2faae68be8ff66bf279f7c06305364e94b1
SmartHardware <sales@smarthardware.com>
2026-08-07 17:33:57 -0400

    Add configurable full system refresh

--- baiamonte-vineyard/app/config.py
+++ baiamonte-vineyard/app/config.py
@@ -33,6 +33,7 @@ class Settings(BaseSettings):
     gmail_folder: str = "INBOX"
     gmail_allowed_senders: str = "laboratorio@cimalab.it,gabrielefedericistudio@gmail.com"
     gmail_poll_minutes: int = 15
+    full_refresh_minutes: int = 60
     weather_history_url: str = ""
     weather_sync_minutes: int = 15
     gw2000_entity_prefix: str = "gw2000,ecowitt"
